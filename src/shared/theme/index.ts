3020f7a99eea74560d3a1a608f6fc0db911768cb
Talita Vial <talitavialsilva1@gmail.com>
2023-12-22 19:44:16 -0400

    feat: arquivo theme de cores e fonts

---
+++ src/shared/theme/index.ts
@@ -0,0 +1,38 @@
+const theme = {
+    colors: {
+      white: "#FFFFFF",
+  
+      brand: "#DB0000",
+  
+      gray_100: "#E1E1E6",
+      gray_200: "#C4C4CC",
+      gray_300: "#8D8D99",
+      gray_400: "#7C7C8A",
+      gray_500: "#505059",
+      gray_800: "#202024",
+      gray_700: "#29292E",
+      gray_600: "#323238",
+      black_700: "#121214",
+  
+      success: "#00c851",
+      warning: "#ffbb33",
+      info: "#33b5e5",
+      danger: "#ff0000",
+    },
+    font_family: {
+      regular: "Lato_400Regular",
+      bold: "Lato_700Bold",
+      extra_bold: "Lato_900Black",
+    },
+    font_size: {
+      xs: '12px',
+      sm: '14px',
+      md: '16px',
+      lg: '18px',
+      xl: '20px',
+      xxl: '24px',
+      xxxl: '32px',
+    },
+  };
+  
+  export default theme;
