2ca3034a2794514e05e4b6955352de555adb92d1
Talita Vial <talitavialsilva1@gmail.com>
2023-12-22 19:43:57 -0400

    feat: tela de login

---
+++ src/modules/auth/screens/LoginScreen/loginScreen.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import * as S from './styles';
+
+export default function LoginScreen(){
+    return(
+        <S.Container>
+            <Text>Login Screen</Text>
+        </S.Container>
+    )
+}
