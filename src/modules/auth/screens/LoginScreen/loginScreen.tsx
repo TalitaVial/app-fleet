40d2948489c1c8d468c3ae47a86b17be2b67f882
Talita Vial <talitavialsilva1@gmail.com>
2023-12-23 09:52:19 -0400

    feat: add lib form, components de formulario no login

--- src/modules/auth/screens/LoginScreen/loginScreen.tsx
+++ src/modules/auth/screens/LoginScreen/loginScreen.tsx
@@ -1,10 +1,39 @@
-import { Text } from "react-native";
-import * as S from './styles';
+import { useForm } from "react-hook-form";
+import loginBackground from "../../../../shared/assets/images/login-background.png";
+import logo from "../../../../shared/assets/images/logo-and-slogan.png";
+import * as S from "./styles";
+import { TextInputForm } from "../../../../shared/components/TextInputForm";
 
-export default function LoginScreen(){
-    return(
-        <S.Container>
-            <Text>Login Screen</Text>
-        </S.Container>
-    )
+export default function LoginScreen() {
+  const { control, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) =>{
+    console.log(data)
+  }
+
+  return (
+    <S.ImageBackground source={loginBackground}>
+      <S.Container>
+        <S.LogoContainer>
+          <S.Logo source={logo} />
+        </S.LogoContainer>
+      </S.Container>
+      <S.FormContainer>
+        <S.TitleForm>Acesse sua conta</S.TitleForm>
+        <TextInputForm
+            control={control}
+            name="email"
+            label="E-mail"
+            keyboardType="email-address"
+         />
+         <TextInputForm
+            control={control}
+            name="password"
+            label="Senha"
+            secureTextEntry
+         />
+         <S.ButtonLogin  title="Entrar" onPress={handleSubmit(onSubmit)}/>
+      </S.FormContainer>
+    </S.ImageBackground>
+  );
 }
--- src/modules/auth/screens/LoginScreen/styles.ts
+++ src/modules/auth/screens/LoginScreen/styles.ts
@@ -1,9 +1,44 @@
-import styled from "styled-components/native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import styled from "styled-components/native";
+import { Button } from "../../../../shared/components/Button";
 
-export const Container = styled(SafeAreaView)`
+export const ImageBackground = styled.ImageBackground`
     flex: 1;
     background-color: ${({theme})=> theme.colors.black_700};
+    height: 60%;
+
 `;
 
+export const Container = styled(SafeAreaView)`
+    /* flex: 1; */
+`;
 
+export const LogoContainer = styled.View`
+    align-items: center;
+    justify-content: center;
+    height: 250px;
+
+`;
+
+export const Logo= styled.Image`
+    align-items: center;
+`;
+
+export const FormContainer = styled.View`
+    padding: 0 30px;
+    gap: 20px;
+
+`;
+
+export const TitleForm = styled.Text`
+    color: ${({theme})=> theme.colors.gray_100};
+    font-size: ${({theme})=> theme.font_size.xl};
+    font-family: ${({theme})=> theme.font_family.bold};
+    text-align: center;
+
+`;
+
+export const ButtonLogin = styled(Button)`
+    margin-top: 20px;
+
+`;
