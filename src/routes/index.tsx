47c656b381387f297c702ba98bf425e069e29315
Talita Vial <talitavialsilva1@gmail.com>
2024-01-15 20:59:00 -0400

    feat: privar rotas com base na autenticação do usuário

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,14 @@
+import { useAuthStore } from "../shared/store/useAuthStore";
+import PrivateRoutes from "./private.routes";
 import PublicRoutes from "./public.routes";
 
 export default function Routes(){
-    return <PublicRoutes/>
+    const user = useAuthStore(state => state.user);
+    console.log({user})
+    //const token = useAuthStore(state => state.token);
+    return (
+        <>
+        {user?.id ? <PrivateRoutes /> : <PublicRoutes/>}
+        </>
+    )
 }
